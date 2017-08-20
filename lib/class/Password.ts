9b8361c9dc774e8acbc0ce2cb5462379f7124466
Diego Islas Ocampo <hyuchia@gmail.com>
2017-08-19 21:54:51 -0500

    Add compare function

--- lib/class/Password.ts
+++ lib/class/Password.ts
@@ -1,22 +1,17 @@
 // Load the bcrypt module
 var bcrypt = require('bcrypt');
-// Generate a salt
-var salt = bcrypt.genSaltSync(10);
-// Hash the password with the salt
-var hash = bcrypt.hashSync("my password", salt);
-
-// Finally just store the hash in your DB
-// .. code to store in Redis/Mongo/Mysql/Sqlite/Postgres/etc.
-
-
 
 export default class Password {
 
 	public static rounds: number = 10;
 
-	public static hash (password: string) {
+	public static hash (password: string): string {
 		let salt = bcrypt.genSaltSync(Password.rounds);
 		return bcrypt.hashSync(password, salt)
+	}
+
+	public static compare (password: string, hash: string): boolean {
+		return bcrypt.compareSync(password, hash);
 	}
 
 }
